dd26a797d1e8ac8431467f0a5a8429356085f867
VarnerDamascenoJr <varnerdjr@gmail.com>
2023-11-26 09:30:43 -0300

    sidebar for the application

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,16 +1,17 @@
 import './sidebar.scss'
+
 import DashboardIcon from '@mui/icons-material/Dashboard';
-import {PersonOutlineOutlinedIcon as UserIcon} from '@mui/icons-material/PersonOutlineOutlined';
-import {StoreOutlinedIcon as ProductsIcon} from '@mui/icons-material/StoreOutlined';
-import {CreditCardOutlinedIcon as CreditCardIcon} from '@mui/icons-material/CreditCardOutlined';
-import {LocalShippingOutlinedIcon as DeliveryIcon} from '@mui/icons-material/LocalShippingOutlined';
-import {AssessmentIcon as StatsIcon} from '@mui/icons-material/Assessment';
-import {NotificationsOutlinedIcon as NotificationsIcon} from '@mui/icons-material/NotificationsOutlined';
-import {MonitorHeartOutlinedIcon as HealthIcon} from '@mui/icons-material/MonitorHeartOutlined';
-import {PsychologyOutlinedIcond as LogsIcon} from '@mui/icons-material/PsychologyOutlined';
-import {SettingsApplicationsIcon as SettingsIcon} from '@mui/icons-material/SettingsApplications';
-import {AccountCircleOutlinedIcon as ProfileIcon} from '@mui/icons-material/AccountCircleOutlined';
-import {ExitToAppOutlinedIcon as LogoutIcon} from '@mui/icons-material/ExitToAppOutlined';
+import PersonOutlineOutlinedIcon  from '@mui/icons-material/PersonOutlineOutlined'
+import StoreOutlinedIcon from '@mui/icons-material/StoreOutlined';
+import CreditCardOutlinedIcon  from '@mui/icons-material/CreditCardOutlined';
+import LocalShippingOutlinedIcon  from '@mui/icons-material/LocalShippingOutlined';
+import AssessmentIcon from '@mui/icons-material/Assessment';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import MonitorHeartOutlinedIcon from '@mui/icons-material/MonitorHeartOutlined';
+import PsychologyOutlinedIcond from '@mui/icons-material/PsychologyOutlined';
+import SettingsApplicationsIcon from '@mui/icons-material/SettingsApplications';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import ExitToAppOutlinedIcon from '@mui/icons-material/ExitToAppOutlined';
 const Sidebar = ()=>{
     return(
         <div className="sidebar">
@@ -27,50 +28,50 @@ const Sidebar = ()=>{
                     </li>
                     <p className="title">Lists</p>
                     <li>
-                        <UserIcon className='icon'/>
+                        <PersonOutlineOutlinedIcon className='icon'/>
                         <span>Users</span>
                     </li>
                     <li>
-                        <ProductsIcon className='icon'/>
+                        <StoreOutlinedIcon className='icon'/>
                         <span>Products</span>
                     </li>
                     <li>
-                        <CreditCardIcon className='icon'/>
+                        <CreditCardOutlinedIcon className='icon'/>
                         <span>Orders</span>
                     </li>
                     <li>
-                        <DeliveryIcon className='icon'/>
+                        <LocalShippingOutlinedIcon className='icon'/>
                         <span>Delivery</span>
                     </li>
                     <p className="title">Useful</p>
                     <li>
-                        <StatsIcon className='icon'/>
+                        <AssessmentIcon className='icon'/>
                         <span>Stats</span>
                     </li>
                     <li>
-                        <NotificationsIcon className='icon'/>
+                        <NotificationsOutlinedIcon className='icon'/>
                         <span>Notifications</span>
                     </li>
                     <p className="title">Service</p>
                     <li>
-                        <HealthIcon className='icon'/>
+                        <MonitorHeartOutlinedIcon className='icon'/>
                         <span>System Health</span>
                     </li>
                     <li>
-                        <LogsIcon className='icon'/>
+                        <PsychologyOutlinedIcond className='icon'/>
                         <span>Logs</span>
                     </li>
                     <li>
-                        <SettingsIcon className='icon'/>
+                        <SettingsApplicationsIcon className='icon'/>
                         <span>Settings</span>
                     </li>
                     <p className="title">User</p>
                     <li>
-                        <ProfileIcon className='icon'/>
+                        <AccountCircleOutlinedIcon className='icon'/>
                         <span>Profile</span>
                     </li>
                     <li>
-                        <LogoutIcon className='icon'/>
+                        <ExitToAppOutlinedIcon className='icon'/>
                         <span>Logout</span>
                     </li>
                 </ul>
